abd7bad97e53fb2e9c025b6d544d4f16d85ea62f
HartantoLs <117240954+HartantoLs@users.noreply.github.com>
2024-10-23 18:08:24 +0700

    lo

---
+++ docs/materiRev/materiRev.js
@@ -0,0 +1,14 @@
+document.getElementById("materiGerak").onclick = function() {
+    window.location.href = "../bookFis/bookFis.html";
+    
+};
+
+document.getElementById("videoGerak").onclick = function() {
+    window.location.href = "../videoFis/videoFis.html";
+    
+};
+
+document.getElementById("eksperimenGerak").onclick = function() {
+    window.location.href = "../eksperimenFisika/eksperimenFisika.html";
+    
+};
