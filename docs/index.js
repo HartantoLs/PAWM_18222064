a1bc96b29485b6b2c486563e98c56f5e925078b2
HartantoLs <117240954+HartantoLs@users.noreply.github.com>
2024-10-23 07:29:05 +0700

    b

--- docs/index.js
+++ docs/index.js
@@ -19,5 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 document.getElementById("playButton").onclick = function() {
+    alert("LOL");
     window.location.href = "../materiRev/materiRev.html"; 
 };
